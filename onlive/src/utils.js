80b5a1962c45838575cd894fc5cd771b3be3887c
cdeath <github@nunopereira.net>
2020-06-15 23:48:57 +0100

    classnames dedup

--- onlive/src/utils.js
+++ onlive/src/utils.js
@@ -1,12 +1,17 @@
+// utils by @cdeath
+
+// what typeof should be
 export function typeOf(prop, type = '') {
     const str = Object.prototype.toString.call(prop).slice(8, -1).toLowerCase();
     return type ? str === type.toLowerCase() : str;
 }
 
+// minimalistic one-liner version of https://www.npmjs.com/package/classnames
 export function classNames(...rest) {
-    return rest.filter(c => !!c).map(c => typeOf(c, 'object') ? Object.keys(c).filter(k => !!c[k]).join(' ') : c.toString()).join(' ');
+    return [...new Set(rest.filter(c => !!c).map(c => typeOf(c, 'object') ? Object.keys(c).filter(k => !!c[k]).join(' ') : c.toString()).filter(c => !!c))].join(' ');
 }
 
+// inpired by https://lodash.com/docs/#padStart
 export function padStart(str = '', len = 0, char = '0') {
     return `${Array(Math.max(0, len - str.toString().length)).fill(char).join('')}${str}`;
 }
